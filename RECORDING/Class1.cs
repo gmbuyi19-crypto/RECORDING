bcd9d73fb20354ebeb186aef929a5858aff564c4
Student <Student@LabVM2047644>
2026-08-07 08:44:28 +0200

    Add project files.

---
+++ RECORDING/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RECORDING
+{
+    public class Response_Sytem
+
+    {
+        public Response_Sytem() { 
+        
+        
+         }
+    }
+}
---
+++ RECORDING/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using RECORDING;
+using System.Media;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace RECORDING
+{
+   
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SoundPlayer player = new SoundPlayer("C:\\Users\\Student\\source\\repos\\RECORDING\\RECORDING\\Properties\\voice\\chatbot_recording.wav");
+            player.PlaySync();
+
+            Console.WriteLine("Voice played successfully!");
+            //Console.ReadLine();
+
+            //ASCII LOGO
+
+            logo logo1 = new logo();
+
+            logo1.DisplayLogo();
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Welcome to the Cybersecurity AwarenessBot!");
+            Console.ResetColor();
+            Console.ReadLine();
+        }
+    }
+}
